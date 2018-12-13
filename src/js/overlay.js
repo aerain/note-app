0d46cac1b70703987299f56869987368d97f329b
aerain <j40f893@gmail.com>
2018-12-13 18:00:54 +0900

    리스트

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -1,25 +1,22 @@
 class AboutOverlay {
     constructor(aboutElement, overlay) {
         this.isVisible = false;
+
+        this.aboutElement = aboutElement;
         this.overlay = overlay;
-        this.content = overlay.querySelector('.about-layer')
+        this.content = overlay.querySelector('.about-layer');
+        this.exitButton = overlay.querySelector('.overlay-exit');
         
 
         this.bindMethod = this.bindMethod.bind(this);
         this.bindMethod();
         
-        this.aboutElement = aboutElement;
-        this.aboutElement.addEventListener('click', this.pressAbout);
-
-        this.setAboutEventListener(aboutElement);
-        this.setAboutOverLay(aboutOverlay);
-        this.setAboutElement(aboutElement);
+        this.aboutElement.addEventListener('click', this.toggleOverlay);
+        this.exitButton.addEventListener('click', this.toggleOverlay);
     }
 
     bindMethod() {
         this.toggleOverlay = this.toggleOverlay.bind(this);
-        this.setAboutEventListener = this.setAboutEventListener.bind(this);
-        this.pressAbout = this.pressAbout.bind(this);
     }
 
     toggleOverlay() {
@@ -29,33 +26,5 @@ class AboutOverlay {
             this.overlay.classList.remove('overlay-toggle');
         }
         this.isVisible = !this.isVisible;
-    }
-
-    setAboutElement(aboutElement) {
-        this.aboutElement = aboutElement;
-        if(aboutElement === null) {
-            if(!this.aboutElement) {
-                this.aboutElement = aboutElement;
-            } else {
-                console.log('올바른 요소가 들어가지 않았습니다.');
-            }
-        } else {
-            
-        }       
-    }
-
-    setAboutEventListener(aboutElement) {
-        if(this.aboutElement) {
-            this.aboutElement.removeEventListener('click', this.pressAbout);
-        }
-        
-    }
-
-    pressAbout(event) {
-        if(this.aboutOverlay !== null) {
-            this.toggleOverlay();
-        } else {
-            console.log('오버레이 할당이 되지 않았습니다.');
-        }
     }
 }
