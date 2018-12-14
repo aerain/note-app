84b39b18fb93440a8c26cd727486720fa5d706ff
aerain <j40f893@gmail.com>
2018-12-15 05:01:11 +0900

    모달 구현

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,8 @@ var textarea = document.querySelector('#memo');
 var newNote = document.querySelector('.btn-newnote');
 var saveNote = document.querySelector('.btn-savenote-to-storage');
 var savePCNote = document.querySelector('.btn-savenote');
+var modal = document.querySelector('.modal');
 
 const header = new FullScreen(fullScreenButton);
 const overlay = new AboutOverlay(aboutButton, aboutOverlay);
-const note = new Note(textarea, newNote, saveNote, savePCNote);
+const note = new Note(textarea, newNote, saveNote, savePCNote, modal);
--- src/js/note.js
+++ src/js/note.js
@@ -1,9 +1,10 @@
 class Note {
-    constructor(noteContent, newNoteButtonElement, saveNoteToStorageButtonElement ,saveNoteButtonElement) {
+    constructor(noteContent, newNoteButtonElement, saveNoteToStorageButtonElement ,saveNoteButtonElement, modalElement) {
         this.noteContent = noteContent;
         this.newNoteButtonElement = newNoteButtonElement;
         this.saveNoteToStorageButtonElement = saveNoteToStorageButtonElement;
         this.saveNoteButtonElement = saveNoteButtonElement;
+        this.modalElement = modalElement;
 
         this.bindMethod = this.bindMethod.bind(this);
         this.bindMethod();
@@ -15,16 +16,25 @@ class Note {
         this.newNote = this.newNote.bind(this);
         this.saveNoteToLocalStorage = this.saveNoteToLocalStorage.bind(this);
         this.saveNoteToPC = this.saveNoteToPC.bind(this);
+        this.modalEvent = this.modalEvent.bind(this);
+        this.modalYes = this.modalYes.bind(this);
     }
 
     setEventListener() {
         this.newNoteButtonElement.addEventListener('click', this.newNote);
         this.saveNoteToStorageButtonElement.addEventListener('click', this.saveNoteToLocalStorage)
         this.saveNoteButtonElement.addEventListener('click', this.saveNoteToPC);
+        this.modalElement.addEventListener('click', this.modalEvent);
+        
     }
 
     newNote(event) {
-        console.log('babo');
+        if(this.noteContent.value !== "") {
+            console.log("뭐");
+            this.modalElement.classList.add('has-text');
+        } else {
+            this.modalEvent();
+        }
     }   
 
     saveNoteToLocalStorage(event) {
@@ -35,6 +45,16 @@ class Note {
         console.log('저장')
     }
 
-
+    modalYes() {
+        this.noteContent.value = "";
+    }
+    modalEvent(event) {
+        let touchedElement = event.target;
+        if(touchedElement.className === "modal has-text" || touchedElement.className === "modal-no") {
+            this.modalElement.classList.remove('has-text');
+        } else if(touchedElement.className === "modal-yes") {
+            this.modalYes();
+        }
+    }
 
 }
